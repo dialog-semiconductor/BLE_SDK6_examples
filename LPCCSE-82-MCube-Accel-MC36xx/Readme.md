494b2f69e306f9e56c995c39fd35cf92abb5a12b
lchandra <leepeng.chandra@diasemi.com>
2019-04-22 16:54:06 -0700

    LC: lpccse-82 inital commit

---
+++ LPCCSE-82-MCube-Accel-MC36xx/Readme.md
@@ -0,0 +1,105 @@
+
+# DA14585/6 interface with mCube Accelerometer over I2C
+
+
+## Example description
+
+Sample software application to interface Dialog DA14585/6 BLE SoC's with mCube MC36xx accelerometers.
+
+This DA14585 example shows how to communicate with a mCube accelerometer sensor over I2C interface. The sensor data gets sent over BLE with a notification when connected to a mobile application. Features in this example includes the implementation code for sensor sniff mode for low-power application and FIFO as sensor buffer.
+
+
+## HW and SW configuration
+
+### Hardware configuration
+
+The current example focuses on the implementation of Accel connected via I2C and sending sensor data over BLE notifications. For SPI interface implementation refer to MC3672 Quick Start Guide which is dedicated for Aurdino demonstration or mCube-MC36XX-Programming Guide for general platforms.
+
+**Note:** Both the SPI and I2C interfaces cannot be active at the same time as the clock (SCK) and data (SDA) are shared between the two protocols.
+
+- This SW example runs on The DA14585/DA14586 Bluetooth Smart SoC devices.
+- The Basic or Pro Development kit is needed for this example.
+- Connect the SCL pin of the EV3672A to pin 1-0 of the development board.
+- Connect the SDA pin of the EV3672A to pin 1-2 of the development board.
+- These pins are defined in the m_drv_interface.h file
+
+<p align="center">
+	<img src="assets/Hardware_setup_DA14585_MC36xx.jpg">
+</p>
+
+### Software configuration
+
+Before user starts to load this DEMO. The following software tools has to be setup.
+- This example requires:
+	- Keil μVision 5
+	- Dialog SDK 6.0.10
+	- Smartphone with a BLE scanning app (for example BLE Scanner on Android or Lightblue on IOS)
+	- **SEGGER’s J-Link** tools should be downloaded and installed.
+
+
+## How to run the example
+
+The implementation of driver is described in the mCube-MC36XX-Programming Guide included in the repository (link below) for customers using mCube sensors for the first time. Refer to the document for understanding the driver implementation. The repository also includes the driver source code if needed.
+
+[Accelerometer mc36xx mcu driver](https://github.com/mcubemems/Accelerometer_mc36xx_mcu_driver)
+
+### Compile and run
+
+To compile and run this program, user can follow the steps below.
+- Open the project mCube_mc36xx_data_notifcation in Keil μVision 5.
+- Optionally, change the parameters in m_drv_mc36xx.c
+- Compile and run the project
+- Open the BLE scanner app and look for DLG-ACCL
+- Connect to the device
+- Subscribe to the notifications
+
+<p align="center"> 
+	<img src="assets/BLE_Notif_DA14585_MC36xx.jpg">
+</p>
+
+- To enable the Sniff and/or FIFO modes, modify the configurations as show below. The default FIFO threshold is 5 samples but it can set to a maximum of 32 samples or just set FIFO to FIFO_FULL mode.
+
+>
+	//#define SNIFF_DEMO //Remove this comment to show the sniff DEMO
+
+	//#define FIFO_DEMO //Remove this comment to show the FIFO DEMO
+
+	#define FIFO_DEEP 32
+
+	#define FIFO_THRESHOLD 5
+
+
+## Known Limitations
+
+
+- There are No known limitations for this example. But you can check and refer to the following application note for
+[known hardware limitations](https://support.dialog-semiconductor.com/system/files/resources/DA1458x-KnownLimitations_2018_02_06.pdf "known hardware limitations").
+- Dialog Software [Forum link](https://support.dialog-semiconductor.com/forums/dialog-smartbond-bluetooth-low-energy-%E2%80%93-software"Forum link").
+- you can Refer also for the Troubleshooting section in the DA1585x Getting Started with the Development Kit UM-B-049.
+
+
+## License
+
+
+**************************************************************************************
+
+ Copyright (c) 2018 Dialog Semiconductor. All rights reserved.
+
+ This software ("Software") is owned by Dialog Semiconductor. By using this Software
+ you agree that Dialog Semiconductor retains all intellectual property and proprietary
+ rights in and to this Software and any use, reproduction, disclosure or distribution
+ of the Software without express written permission or a license agreement from Dialog
+ Semiconductor is strictly prohibited. This Software is solely for use on or in
+ conjunction with Dialog Semiconductor products.
+
+ EXCEPT AS OTHERWISE PROVIDED IN A LICENSE AGREEMENT BETWEEN THE PARTIES OR AS
+ REQUIRED BY LAW, THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. EXCEPT AS OTHERWISE PROVIDED
+ IN A LICENSE AGREEMENT BETWEEN THE PARTIES OR BY LAW, IN NO EVENT SHALL DIALOG
+ SEMICONDUCTOR BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT, INCIDENTAL, OR
+ CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THE SOFTWARE.
+
+**************************************************************************************
---
+++ LPCCSE-82-MCube-Accel-MC36xx/src/platform/user_periph_setup.c
@@ -0,0 +1,115 @@
+/**
+ ****************************************************************************************
+ *
+ * @file user_periph_setup.c
+ *
+ * @brief Peripherals setup and initialization.
+ *
+ * Copyright (C) 2018 Dialog Semiconductor. This computer program or computer programs included in this package ("Software") include confidential, proprietary information of Dialog Semiconductor. All Rights Reserved.
+ *
+ * THIS SOFTWARE IS AN UNOFFICIAL RELEASE FROM DIALOG SEMICONDUCTOR (‘DIALOG’) AND MAY ONLY BE USED BY RECIPIENT AT ITS OWN RISK AND WITHOUT SUPPORT OF ANY KIND.  THIS SOFTWARE IS SOLELY FOR USE ON AUTHORIZED DIALOG PRODUCTS AND PLATFORMS.  RECIPIENT SHALL NOT TRANSMIT ANY SOFTWARE SOURCE CODE TO ANY THIRD PARTY WITHOUT DIALOG’S PRIOR WRITTEN PERMISSION.
+ *
+ * UNLESS SET FORTH IN A SEPARATE AGREEMENT, RECIPIENT ACKNOWLEDGES AND UNDERSTANDS THAT TO THE FULLEST EXTENT PERMITTED BY LAW, THE SOFTWARE IS DELIVERED “AS IS”, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, ANY IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABILITY, TITLE OR NON-INFRINGEMENT, AND ALL WARRANTIES THAT MAY ARISE FROM COURSE OF DEALING, CUSTOM OR USAGE IN TRADE. FOR THE SAKE OF CLARITY, DIALOG AND ITS AFFILIATES AND ITS AND THEIR SUPPLIERS DO NOT WARRANT, GUARANTEE OR MAKE ANY REPRESENTATIONS (A) REGARDING THE USE, OR THE RESULTS OF THE USE, OF THE LICENSED SOFTWARE IN TERMS OF CORRECTNESS, COMPLETENESS, ACCURACY, RELIABILITY OR OTHERWISE, AND (B) THAT THE LICENSED SOFTWARE HAS BEEN TESTED FOR COMPLIANCE WITH ANY REGULATORY OR INDUSTRY STANDARD, INCLUDING, WITHOUT LIMITATION, ANY SUCH STANDARDS PROMULGATED BY THE FCC OR OTHER LIKE AGENCIES.
+ *
+ * IN NO EVENT SHALL DIALOG BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ ****************************************************************************************
+ */
+
+/*
+ * INCLUDE FILES
+ ****************************************************************************************
+ */
+
+#include "rwip_config.h"             // SW configuration
+#include "user_periph_setup.h"       // peripheral configuration
+#include "gpio.h"
+#include "uart.h"                    // UART initialization
+#include "m_drv_console.h"
+#include "m_drv_interface.h"
+#include "m_drv_mc36xx.h"
+#include "arch_console.h"
+#include "user_accelerometer.h"
+
+#if DEVELOPMENT_DEBUG
+
+void GPIO_reservations(void)
+{
+/*
+* Globally reserved GPIOs reservation
+*/
+
+/*
+* Application specific GPIOs reservation. Used only in Development mode (#if DEVELOPMENT_DEBUG)
+
+i.e.
+    RESERVE_GPIO(DESCRIPTIVE_NAME, GPIO_PORT_0, GPIO_PIN_1, PID_GPIO);    //Reserve P_01 as Generic Purpose I/O
+*/
+
+#ifdef CFG_PRINTF_UART2
+    RESERVE_GPIO(UART2_TX, UART2_TX_GPIO_PORT, UART2_TX_GPIO_PIN, PID_UART2_TX);
+    RESERVE_GPIO(UART2_RX, UART2_RX_GPIO_PORT, UART2_RX_GPIO_PIN, PID_UART2_RX);
+#endif
+}
+#endif //DEVELOPMENT_DEBUG
+
+void set_pad_functions(void)        // set gpio port function mode
+{
+/*
+* Configure application ports.
+i.e.
+    GPIO_ConfigurePin( GPIO_PORT_0, GPIO_PIN_1, OUTPUT, PID_GPIO, false ); // Set P_01 as Generic purpose Output
+*/
+
+#ifdef __DA14586__
+    // disallow spontaneous flash wake-up
+    GPIO_ConfigurePin(SPI_EN_GPIO_PORT, SPI_EN_GPIO_PIN, OUTPUT, PID_GPIO, true);
+#endif
+
+#ifdef CFG_PRINTF_UART2
+    GPIO_ConfigurePin(UART2_TX_GPIO_PORT, UART2_TX_GPIO_PIN, OUTPUT, PID_UART2_TX, false);
+    GPIO_ConfigurePin(UART2_RX_GPIO_PORT, UART2_RX_GPIO_PIN, INPUT, PID_UART2_RX, false);
+#endif
+}
+
+void periph_init(void)
+{
+    // Power up peripherals' power domain
+    SetBits16(PMU_CTRL_REG, PERIPH_SLEEP, 0);
+    while (!(GetWord16(SYS_STAT_REG) & PER_IS_UP));
+
+    SetBits16(CLK_16M_REG, XTAL16_BIAS_SH_ENABLE, 1);
+
+    //rom patch
+    patch_func();
+
+    //Init pads
+    set_pad_functions();
+
+    // (Re)Initialize peripherals
+    // i.e.
+    //  uart_init(UART_BAUDRATE_115K2, UART_FRAC_BAUDRATE_115K2, UART_CHARFORMAT_8);
+
+#ifdef CFG_PRINTF_UART2
+    SetBits16(CLK_PER_REG, UART2_ENABLE, 1);
+    uart2_init(UART_BAUDRATE_115K2, UART_FRAC_BAUDRATE_115K2, UART_CHARFORMAT_8);
+#endif
+
+   // Enable the pads
+    SetBits16(SYS_CTRL_REG, PAD_LATCH_EN, 1);
+
+    RESERVE_GPIO(,I2C_GPIO_PORT, I2C_SDA_PIN, PID_I2C_SDA);
+    RESERVE_GPIO(,I2C_GPIO_PORT, I2C_SCL_PIN, PID_I2C_SCL);
+    // Init I2C
+    GPIO_ConfigurePin(I2C_GPIO_PORT, I2C_SCL_PIN, INPUT, PID_I2C_SCL, false);
+    GPIO_ConfigurePin(I2C_GPIO_PORT, I2C_SDA_PIN, INPUT, PID_I2C_SDA, false);
+
+    m_drv_i2c_init();
+
+    M_DRV_MC36XX_Init();
+#ifdef FIFO_DEMO
+    M_DRV_MC36XX_EnableFIFO(E_M_DRV_MC36XX_FIFO_CTL_ENABLE, E_M_DRV_MC36XX_FIFO_MODE_WATERMARK, FIFO_THRESHOLD);
+#endif
+    M_DRV_MC36XX_SetMode(E_M_DRV_MC36XX_MODE_CWAKE);
+}

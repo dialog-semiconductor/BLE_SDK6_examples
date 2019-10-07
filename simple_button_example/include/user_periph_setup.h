615971f88dc57f0d089723ba9f0aeb78bceecbeb
Terence Beijloos <terence.beijloos@diasemi.com>
2019-10-07 14:00:41 +0200

    Made a new branch and copied the simple_button example to this branch. This is done because a pull request can now be made. Also the python script was changed to 1.006, from 1.004.

---
+++ simple_button_example/include/user_periph_setup.h
@@ -0,0 +1,99 @@
+/**
+ ****************************************************************************************
+ *
+ * @file user_periph_setup.h
+ *
+ * @brief Peripherals setup header file.
+ *
+ * Copyright (c) 2015-2018 Dialog Semiconductor. All rights reserved.
+ *
+ * This software ("Software") is owned by Dialog Semiconductor.
+ *
+ * By using this Software you agree that Dialog Semiconductor retains all
+ * intellectual property and proprietary rights in and to this Software and any
+ * use, reproduction, disclosure or distribution of the Software without express
+ * written permission or a license agreement from Dialog Semiconductor is
+ * strictly prohibited. This Software is solely for use on or in conjunction
+ * with Dialog Semiconductor products.
+ *
+ * EXCEPT AS OTHERWISE PROVIDED IN A LICENSE AGREEMENT BETWEEN THE PARTIES, THE
+ * SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. EXCEPT AS OTHERWISE
+ * PROVIDED IN A LICENSE AGREEMENT BETWEEN THE PARTIES, IN NO EVENT SHALL
+ * DIALOG SEMICONDUCTOR BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT, INCIDENTAL,
+ * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF
+ * USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+ * OF THE SOFTWARE.
+ *
+ ****************************************************************************************
+ */
+
+#ifndef _USER_PERIPH_SETUP_H_
+#define _USER_PERIPH_SETUP_H_
+
+/*
+ * INCLUDE FILES
+ ****************************************************************************************
+ */
+
+#include "uart.h"
+#include "gpio.h"
+
+/****************************************************************************************/
+/* UART2 configuration to print messages                                                */
+/****************************************************************************************/
+#define UART                        UART2
+
+#if defined (__DA14531__)
+    #define UART2_TX_PORT           GPIO_PORT_0
+    #define UART2_TX_PIN            GPIO_PIN_6
+#else
+    #define UART2_TX_PORT           GPIO_PORT_0
+    #define UART2_TX_PIN            GPIO_PIN_4
+#endif
+
+// Define UART2 Settings
+#define UART2_BAUDRATE              UART_BAUDRATE_115200
+#define UART2_DATABITS              UART_DATABITS_8
+#define UART2_PARITY                UART_PARITY_NONE
+#define UART2_STOPBITS              UART_STOPBITS_1
+#define UART2_AFCE                  UART_AFCE_DIS
+#define UART2_FIFO                  UART_FIFO_EN
+#define UART2_TX_FIFO_LEVEL         UART_TX_FIFO_LEVEL_0
+#define UART2_RX_FIFO_LEVEL         UART_RX_FIFO_LEVEL_0
+
+/****************************************************************************************/
+/* LED configuration                                                                    */
+/****************************************************************************************/
+#if defined (__DA14531__)
+    #define LED_PORT                GPIO_PORT_0
+    #define LED_PIN                 GPIO_PIN_9
+#else
+    #define LED_PORT                GPIO_PORT_1
+    #define LED_PIN                 GPIO_PIN_0
+#endif
+	
+	#if defined (__DA14531__)
+		#define GPIO_SW3_PORT			 GPIO_PORT_0 
+		#define GPIO_SW3_PIN			 GPIO_PIN_8
+#else
+		#define GPIO_SW3_PORT			 GPIO_PORT_1 
+		#define GPIO_SW3_PIN			 GPIO_PIN_1 
+#endif
+
+/*
+ * FUNCTION DECLARATIONS
+ ****************************************************************************************
+ */
+
+/**
+ ****************************************************************************************
+ * @brief   Initializes application's peripherals and pins
+ * @return  void
+ ****************************************************************************************
+ */
+void periph_init(void);
+
+#endif // _USER_PERIPH_SETUP_H_
---
+++ simple_button_example/src/user_periph_setup.c
@@ -0,0 +1,82 @@
+/**
+ ****************************************************************************************
+ *
+ * @file periph_setup.c
+ *
+ * @brief Peripherals initialization functions
+ *
+ * Copyright (c) 2012-2019 Dialog Semiconductor. All rights reserved.
+ *
+ * This software ("Software") is owned by Dialog Semiconductor.
+ *
+ * By using this Software you agree that Dialog Semiconductor retains all
+ * intellectual property and proprietary rights in and to this Software and any
+ * use, reproduction, disclosure or distribution of the Software without express
+ * written permission or a license agreement from Dialog Semiconductor is
+ * strictly prohibited. This Software is solely for use on or in conjunction
+ * with Dialog Semiconductor products.
+ *
+ * EXCEPT AS OTHERWISE PROVIDED IN A LICENSE AGREEMENT BETWEEN THE PARTIES, THE
+ * SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. EXCEPT AS OTHERWISE
+ * PROVIDED IN A LICENSE AGREEMENT BETWEEN THE PARTIES, IN NO EVENT SHALL
+ * DIALOG SEMICONDUCTOR BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT, INCIDENTAL,
+ * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF
+ * USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+ * OF THE SOFTWARE.
+ *
+ ****************************************************************************************
+ */
+
+/*
+ * INCLUDE FILES
+ ****************************************************************************************
+ */
+
+#include "user_periph_setup.h"
+#include "datasheet.h"
+
+static void set_pad_functions(void)
+{
+#if defined (__DA14586__)
+    // Disallow spontaneous DA14586 SPI Flash wake-up
+    GPIO_ConfigurePin(GPIO_PORT_2, GPIO_PIN_3, OUTPUT, PID_GPIO, true);
+#endif
+
+		GPIO_ConfigurePin(GPIO_SW3_PORT, GPIO_SW3_PIN, INPUT_PULLUP, PID_GPIO, false);
+	
+    GPIO_ConfigurePin(UART2_TX_PORT, UART2_TX_PIN, OUTPUT, PID_UART2_TX, false);
+
+    GPIO_ConfigurePin(LED_PORT, LED_PIN, OUTPUT, PID_GPIO, false);
+}
+
+// Configuration struct for UART2
+static const uart_cfg_t uart_cfg = {
+    .baud_rate = UART2_BAUDRATE,
+    .data_bits = UART2_DATABITS,
+    .parity = UART2_PARITY,
+    .stop_bits = UART2_STOPBITS,
+    .auto_flow_control = UART2_AFCE,
+    .use_fifo = UART2_FIFO,
+    .tx_fifo_tr_lvl = UART2_TX_FIFO_LEVEL,
+    .rx_fifo_tr_lvl = UART2_RX_FIFO_LEVEL,
+    .intr_priority = 2,
+};
+
+void periph_init(void)
+{
+#if !defined (__DA14531__)
+    // Power up peripherals' power domain
+    SetBits16(PMU_CTRL_REG, PERIPH_SLEEP, 0);
+    while (!(GetWord16(SYS_STAT_REG) & PER_IS_UP));
+    SetBits16(CLK_16M_REG, XTAL16_BIAS_SH_ENABLE, 1);
+#endif
+	
+    uart_initialize(UART2, &uart_cfg);
+
+    set_pad_functions();
+
+    GPIO_set_pad_latch_en(true);
+}

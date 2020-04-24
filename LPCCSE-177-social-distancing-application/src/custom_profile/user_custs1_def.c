4741e0247226e161486926f93d6de186ecbd7320
Thanos Tsakiris <ttsakiri@diasemi.com>
2020-04-24 14:35:26 +0300

    Remove unused services

--- LPCCSE-177-social-distancing-application/src/custom_profile/user_custs1_def.c
+++ LPCCSE-177-social-distancing-application/src/custom_profile/user_custs1_def.c
@@ -67,19 +67,6 @@ static const uint8_t SVC1_BUTTON_STATE_UUID_128[ATT_UUID_128_LEN]     = DEF_SVC1
 static const uint8_t SVC1_INDICATEABLE_UUID_128[ATT_UUID_128_LEN]     = DEF_SVC1_INDICATEABLE_UUID_128;
 static const uint8_t SVC1_LONG_VALUE_UUID_128[ATT_UUID_128_LEN]       = DEF_SVC1_LONG_VALUE_UUID_128;
 
-// Service 2 of the custom server 1
-static const att_svc_desc128_t custs1_svc2                      = DEF_SVC2_UUID_128;
-
-static const uint8_t SVC2_WRITE_VAL_1_UUID_128[ATT_UUID_128_LEN]      = DEF_SVC2_WRITE_VAL_1_UUID_128;
-static const uint8_t SVC2_WRITE_VAL_2_UUID_128[ATT_UUID_128_LEN]      = DEF_SVC2_WRITE_VAL_2_UUID_128;
-
-// Service 3 of the custom server 1
-static const att_svc_desc128_t custs1_svc3                      = DEF_SVC3_UUID_128;
-
-static const uint8_t SVC3_READ_VAL_1_UUID_128[ATT_UUID_128_LEN]       = DEF_SVC3_READ_VAL_1_UUID_128;
-static const uint8_t SVC3_READ_VAL_2_UUID_128[ATT_UUID_128_LEN]       = DEF_SVC3_READ_VAL_2_UUID_128;
-static const uint8_t SVC3_READ_VAL_3_UUID_128[ATT_UUID_128_LEN]       = DEF_SVC3_READ_VAL_3_UUID_128;
-
 // Attribute specifications
 static const uint16_t att_decl_svc       = ATT_DECL_PRIMARY_SERVICE;
 static const uint16_t att_decl_char      = ATT_DECL_CHARACTERISTIC;
@@ -91,7 +78,7 @@ static const uint16_t att_desc_user_desc = ATT_DESC_CHAR_USER_DESCRIPTION;
  ****************************************************************************************
  */
 
-const uint8_t custs1_services[]  = {SVC1_IDX_SVC, SVC2_IDX_SVC, SVC3_IDX_SVC, CUSTS1_IDX_NB};
+const uint8_t custs1_services[]  = {SVC1_IDX_SVC, CUSTS1_IDX_NB};
 const uint8_t custs1_services_size = ARRAY_LEN(custs1_services) - 1;
 const uint16_t custs1_att_max_nb = CUSTS1_IDX_NB;
 
@@ -213,92 +200,6 @@ const struct attm_desc_128 custs1_att_db[CUSTS1_IDX_NB] =
     [SVC1_IDX_LONG_VALUE_USER_DESC]    = {(uint8_t*)&att_desc_user_desc, ATT_UUID_16_LEN, PERM(RD, ENABLE),
                                             sizeof(DEF_SVC1_LONG_VALUE_CHAR_USER_DESC) - 1, sizeof(DEF_SVC1_LONG_VALUE_CHAR_USER_DESC) - 1,
                                             (uint8_t *) DEF_SVC1_LONG_VALUE_CHAR_USER_DESC},
-
-    /*************************
-     * Service 2 configuration
-     *************************
-     */
-
-    // Service 2 Declaration
-    [SVC2_IDX_SVC]                     = {(uint8_t*)&att_decl_svc, ATT_UUID_128_LEN, PERM(RD, ENABLE),
-                                            sizeof(custs1_svc2), sizeof(custs1_svc2), (uint8_t*)&custs1_svc2},
-
-    // Write 1 Characteristic Declaration
-    [SVC2_WRITE_1_CHAR]                = {(uint8_t*)&att_decl_char, ATT_UUID_16_LEN, PERM(RD, ENABLE), 0, 0, NULL},
-
-    // Write 1 Characteristic Value
-    [SVC2_WRITE_1_VAL]                 = {SVC2_WRITE_VAL_1_UUID_128, ATT_UUID_128_LEN, PERM(WR, ENABLE) | PERM(WRITE_REQ, ENABLE),
-                                            DEF_SVC2_WRITE_VAL_1_CHAR_LEN, 0, NULL},
-
-    // Write 1 Characteristic User Description
-    [SVC2_WRITE_1_USER_DESC]           = {(uint8_t*)&att_desc_user_desc, ATT_UUID_16_LEN, PERM(RD, ENABLE),
-                                            sizeof(DEF_SVC2_WRITE_VAL_1_USER_DESC) - 1, sizeof(DEF_SVC2_WRITE_VAL_1_USER_DESC) - 1,
-                                            (uint8_t *) DEF_SVC2_WRITE_VAL_1_USER_DESC},
-
-    // Write 2 Characteristic Declaration
-    [SVC2_WRITE_2_CHAR]                = {(uint8_t*)&att_decl_char, ATT_UUID_16_LEN, PERM(RD, ENABLE), 0, 0, NULL},
-
-    // Write 2 Characteristic Value
-    [SVC2_WRITE_2_VAL]                 = {SVC2_WRITE_VAL_2_UUID_128, ATT_UUID_128_LEN, PERM(WR, ENABLE) | PERM(WRITE_COMMAND, ENABLE),
-                                            DEF_SVC2_WRITE_VAL_2_CHAR_LEN, 0, NULL},
-
-    // Write 2 Characteristic User Description
-    [SVC2_WRITE_2_USER_DESC]           = {(uint8_t*)&att_desc_user_desc, ATT_UUID_16_LEN, PERM(RD, ENABLE),
-                                            sizeof(DEF_SVC2_WRITE_VAL_2_USER_DESC) - 1, sizeof(DEF_SVC2_WRITE_VAL_2_USER_DESC) - 1,
-                                            (uint8_t *) DEF_SVC2_WRITE_VAL_2_USER_DESC},
-
-    /*************************
-     * Service 3 configuration
-     *************************
-     */
-
-    // Service 3 Declaration
-    [SVC3_IDX_SVC]                     = {(uint8_t*)&att_decl_svc, ATT_UUID_128_LEN, PERM(RD, ENABLE),
-                                            sizeof(custs1_svc3), sizeof(custs1_svc3), (uint8_t*)&custs1_svc3},
-
-    // Read 1 Characteristic Declaration
-    [SVC3_IDX_READ_1_CHAR]             = {(uint8_t*)&att_decl_char, ATT_UUID_16_LEN, PERM(RD, ENABLE), 0, 0, NULL},
-
-    // Read 1 Characteristic Value
-    [SVC3_IDX_READ_1_VAL]              = {SVC3_READ_VAL_1_UUID_128, ATT_UUID_128_LEN, PERM(RD, ENABLE) | PERM(NTF, ENABLE),
-                                            DEF_SVC3_READ_VAL_1_CHAR_LEN, 0, NULL},
-
-    // Read 1 Client Characteristic Configuration Descriptor
-    [SVC3_IDX_READ_1_NTF_CFG]          = {(uint8_t*)&att_desc_cfg, ATT_UUID_16_LEN, PERM(RD, ENABLE) | PERM(WR, ENABLE) | PERM(WRITE_REQ, ENABLE),
-                                            sizeof(uint16_t), 0, NULL},
-
-    // Read 1 Characteristic User Description
-    [SVC3_IDX_READ_1_USER_DESC]        = {(uint8_t*)&att_desc_user_desc, ATT_UUID_16_LEN, PERM(RD, ENABLE),
-                                            sizeof(DEF_SVC3_READ_VAL_1_USER_DESC) - 1, sizeof(DEF_SVC3_READ_VAL_1_USER_DESC) - 1,
-                                            (uint8_t *) DEF_SVC3_READ_VAL_1_USER_DESC},
-
-    // Read 2 Characteristic Declaration
-    [SVC3_IDX_READ_2_CHAR]             = {(uint8_t*)&att_decl_char, ATT_UUID_16_LEN, PERM(RD, ENABLE), 0, 0, NULL},
-
-    // Read 2 Characteristic Value
-    [SVC3_IDX_READ_2_VAL]              = {SVC3_READ_VAL_2_UUID_128, ATT_UUID_128_LEN, PERM(RD, ENABLE),
-                                            DEF_SVC3_READ_VAL_2_CHAR_LEN, 0, NULL},
-
-    // Read 2 Characteristic User Description
-    [SVC3_IDX_READ_2_USER_DESC]        = {(uint8_t*)&att_desc_user_desc, ATT_UUID_16_LEN, PERM(RD, ENABLE),
-                                            sizeof(DEF_SVC3_READ_VAL_2_USER_DESC) - 1, sizeof(DEF_SVC3_READ_VAL_2_USER_DESC) - 1,
-                                            (uint8_t *) DEF_SVC3_READ_VAL_2_USER_DESC},
-    
-    // Read 3 Characteristic Declaration
-    [SVC3_IDX_READ_3_CHAR]             = {(uint8_t*)&att_decl_char, ATT_UUID_16_LEN, PERM(RD, ENABLE), 0, 0, NULL},
-
-    // Read 3 Characteristic Value
-    [SVC3_IDX_READ_3_VAL]              = {SVC3_READ_VAL_3_UUID_128, ATT_UUID_128_LEN, PERM(RD, ENABLE) | PERM(IND, ENABLE),
-                                            DEF_SVC3_READ_VAL_3_CHAR_LEN, 0, NULL},
-
-    // Read 3 Client Characteristic Configuration Descriptor
-    [SVC3_IDX_READ_3_IND_CFG]           = {(uint8_t*)&att_desc_cfg, ATT_UUID_16_LEN, PERM(RD, ENABLE) | PERM(WR, ENABLE) | PERM(WRITE_REQ, ENABLE),
-                                            sizeof(uint16_t), 0, NULL},
-
-    // Read 3 Characteristic User Description
-    [SVC3_IDX_READ_3_USER_DESC]         = {(uint8_t*)&att_desc_user_desc, ATT_UUID_16_LEN, PERM(RD, ENABLE),
-                                            sizeof(DEF_SVC3_READ_VAL_3_USER_DESC) - 1, sizeof(DEF_SVC3_READ_VAL_3_USER_DESC) - 1,
-                                            (uint8_t *) DEF_SVC3_READ_VAL_3_USER_DESC},
 };
 
 /// @} USER_CONFIG

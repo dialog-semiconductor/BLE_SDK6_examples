ae526cfb78c68e2e88b168b7a1657e8804d65cea
ibenamor <82457185+ibenamor@users.noreply.github.com>
2022-10-10 16:15:30 +0200

    First Draft of the SPI Master-Slave example

---
+++ interfaces/SPI-Master-Slave/SPI_Slave/src/config/user_periph_setup.h
@@ -0,0 +1,103 @@
+/**
+ ****************************************************************************************
+ *
+ * @file user_periph_setup.h
+ *
+ * @brief Peripherals setup header file.
+ *
+ * Copyright (C) 2012-2020 Dialog Semiconductor.
+ * This computer program includes Confidential, Proprietary Information
+ * of Dialog Semiconductor. All Rights Reserved.
+ *
+ ****************************************************************************************
+ */
+
+#ifndef _USER_PERIPH_SETUP_H_
+#define _USER_PERIPH_SETUP_H_
+
+/*
+ * INCLUDE FILES
+ ****************************************************************************************
+ */
+
+#include "uart.h"
+#include "gpio.h"
+#include "spi.h"
+#include "spi_flash.h"
+
+/*
+ * DEFINES
+ ****************************************************************************************
+ */
+
+/****************************************************************************************/
+/* UART2 configuration to print messages                                                */
+/****************************************************************************************/
+#define UART                        UART2
+// Define UART2 Tx Pad
+#if defined (__DA14531__)
+    #define UART2_TX_PORT           GPIO_PORT_0
+    #define UART2_TX_PIN            GPIO_PIN_5
+#else
+    #define UART2_TX_PORT           GPIO_PORT_0
+    #define UART2_TX_PIN            GPIO_PIN_4
+#endif
+
+// Define UART2 Settings
+#define UART2_BAUDRATE              UART_BAUDRATE_115200
+#define UART2_DATABITS              UART_DATABITS_8
+#define UART2_PARITY                UART_PARITY_NONE
+#define UART2_STOPBITS              UART_STOPBITS_1
+#define UART2_AFCE                  UART_AFCE_DIS
+#define UART2_FIFO                  UART_FIFO_EN
+#define UART2_TX_FIFO_LEVEL         UART_TX_FIFO_LEVEL_0
+#define UART2_RX_FIFO_LEVEL         UART_RX_FIFO_LEVEL_0
+
+/****************************************************************************************/
+/* SPI configuration                                                                    */
+/****************************************************************************************/
+// Define SPI Pads for DA14531 target
+
+    #define SPI_EN_PORT             GPIO_PORT_0
+    #define SPI_EN_PIN              GPIO_PIN_8
+
+    #define SPI_CLK_PORT            GPIO_PORT_0
+    #define SPI_CLK_PIN             GPIO_PIN_6
+
+    #define SPI_DO_PORT             GPIO_PORT_0
+    #define SPI_DO_PIN              GPIO_PIN_11
+
+    #define SPI_DI_PORT             GPIO_PORT_0
+    #define SPI_DI_PIN              GPIO_PIN_9
+
+
+// Define SPI Configuration
+    #define SPI_MS_MODE             SPI_MS_MODE_SLAVE
+    #define SPI_CP_MODE             SPI_CP_MODE_0
+    #define SPI_WSZ                 SPI_MODE_8BIT
+    #define SPI_CS                  SPI_CS_0
+
+    #define SPI_SPEED_MODE          SPI_SPEED_MODE_32MHz
+    #define SPI_EDGE_CAPTURE        SPI_MASTER_EDGE_CAPTURE
+
+
+/*
+ * FUNCTION DECLARATIONS
+ ****************************************************************************************
+ */
+
+/**
+ ****************************************************************************************
+ * @brief   Initializes application's peripherals and pins
+ ****************************************************************************************
+ */
+void periph_init(void);
+
+/**
+ ****************************************************************************************
+ * @brief Set gpio port function mode
+ ****************************************************************************************
+ */
+void set_pad_functions(void);
+
+#endif // _USER_PERIPH_SETUP_H_
---
+++ interfaces/SPI-Master-Slave/SPI_Slave/src/main.c
@@ -0,0 +1,113 @@
+/**
+ ****************************************************************************************
+ *
+ * @file main.c
+ *
+ * @brief SPI Slave demo
+ *
+ * Copyright (C) 2012-2022 Dialog Semiconductor.
+ * This computer program includes Confidential, Proprietary Information
+ * of Dialog Semiconductor. All Rights Reserved.
+ *
+ ****************************************************************************************
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include "arch_system.h"
+#include "user_periph_setup.h"
+#include "gpio.h"
+#include "uart.h"
+#include "uart_utils.h"
+#include "spi.h"
+#include "spi_flash.h"
+
+// Settings
+#define SPI_DEMO_MESSAGES_LENGTH (12)
+
+/* buffer with data to send to SPI*/
+const uint8_t spi_demo_out_buffer[SPI_DEMO_MESSAGES_LENGTH] = {'R','e','s','p','o','n','s','e',' ','s','l',0};
+
+/* buffer with data to receive from SPI */
+uint8_t spi_demo_in_buffer[SPI_DEMO_MESSAGES_LENGTH];
+const uint8_t spi_demo_expected_response[SPI_DEMO_MESSAGES_LENGTH] = {'D','i','a','l','o','g',' ','d','e','m','o',0};
+
+
+/**
+ ****************************************************************************************
+ * @brief SPI demo function
+ ****************************************************************************************
+ */
+static void spi_demo(void);
+
+/**
+ ****************************************************************************************
+ * @brief Main routine of the SPI demo
+ ****************************************************************************************
+ */
+int main (void)
+{
+    /* System initialization */
+	  system_init();
+	  spi_demo();
+    while(1);
+}
+
+/**
+ ****************************************************************************************
+ * @brief Callback to return in interrupt/DMA-driven transferring operations
+ * @param[in] spi_cb_t  SPI callback type. 
+ ****************************************************************************************
+ */
+
+void spi_transfer_user_callback(uint16_t spi_transaction_length)
+{
+    uint8_t i;
+    
+    if (strcmp((const char*)spi_demo_in_buffer, (const char*)spi_demo_expected_response) == 0)
+    {
+        printf_string(UART, "\n\rAll bytes received OK\r\n");
+    }
+    else
+    {
+        printf_string(UART, "\n\rInvalid byte(s) received\r\n\r\n");
+    }
+        
+    printf_string(UART, "\n\rBytes received:\r\n  In hex:");
+
+    for (i=0; i<spi_transaction_length; i++)
+    {
+        printf_string(UART, "0x");
+        printf_byte(UART, spi_demo_in_buffer[i]); 
+        printf_string(UART, ", ");        
+    }
+    
+    printf_string(UART, "\n\r  String:");
+    printf_string(UART, (char*)spi_demo_in_buffer);
+}
+
+/**
+ ****************************************************************************************
+ * @brief SPI demo function
+ ****************************************************************************************
+ */
+
+static void spi_demo(void)
+{
+    printf_string(UART, "\n\r*******************");   
+    
+	  printf_string(UART, "\n\r* SPI SLAVE DEMO *");         
+
+    printf_string(UART, "\n\r*******************\n\r");   
+    
+    spi_register_transfer_cb(spi_transfer_user_callback);
+    
+    memchr(spi_demo_in_buffer,'*',SPI_DEMO_MESSAGES_LENGTH);
+    
+    
+	  /* Start sending Data*/
+	  spi_transfer(spi_demo_out_buffer, spi_demo_in_buffer, SPI_DEMO_MESSAGES_LENGTH, SPI_OP_DMA);
+	
+	  while(1);
+
+}

a077e3888ee329d78f8a260f4e07ac759e6bc851
Ben Lauret <ben.lauret@diasemi.com>
2023-02-14 17:09:18 +0100

    Initial empyt peripheral template port

---
+++ features/empty_peripheral_eclipse/src/platform/user_periph_setup.c
@@ -0,0 +1,116 @@
+/**
+ ****************************************************************************************
+ *
+ * @file user_periph_setup.c
+ *
+ * @brief Peripherals setup and initialization.
+ *
+ * Copyright (C) 2015-2019 Dialog Semiconductor.
+ * This computer program includes Confidential, Proprietary Information
+ * of Dialog Semiconductor. All Rights Reserved.
+ *
+ ****************************************************************************************
+ */
+
+/*
+ * INCLUDE FILES
+ ****************************************************************************************
+ */
+
+#include "user_periph_setup.h"
+#include "datasheet.h"
+#include "system_library.h"
+#include "rwip_config.h"
+#include "gpio.h"
+#include "uart.h"
+#include "syscntl.h"
+
+/*
+ * GLOBAL VARIABLE DEFINITIONS
+ ****************************************************************************************
+ */
+
+#if DEVELOPMENT_DEBUG
+
+void GPIO_reservations(void)
+{
+/*
+    i.e. to reserve P0_1 as Generic Purpose I/O:
+    RESERVE_GPIO(DESCRIPTIVE_NAME, GPIO_PORT_0, GPIO_PIN_1, PID_GPIO);
+*/
+
+#if defined (CFG_PRINTF_UART2)
+    RESERVE_GPIO(UART2_TX, UART2_TX_PORT, UART2_TX_PIN, PID_UART2_TX);
+#endif
+
+#if !defined (__DA14586__)
+    RESERVE_GPIO(SPI_EN, SPI_EN_PORT, SPI_EN_PIN, PID_SPI_EN);
+#endif
+}
+
+#endif
+
+void set_pad_functions(void)
+{
+/*
+    i.e. to set P0_1 as Generic purpose Output:
+    GPIO_ConfigurePin(GPIO_PORT_0, GPIO_PIN_1, OUTPUT, PID_GPIO, false);
+*/
+
+#if defined (__DA14586__)
+    // Disallow spontaneous DA14586 SPI Flash wake-up
+    GPIO_ConfigurePin(GPIO_PORT_2, GPIO_PIN_3, OUTPUT, PID_GPIO, true);
+#else
+    // Disallow spontaneous SPI Flash wake-up
+    GPIO_ConfigurePin(SPI_EN_PORT, SPI_EN_PIN, OUTPUT, PID_SPI_EN, true);
+#endif
+
+#if defined (CFG_PRINTF_UART2)
+    // Configure UART2 TX Pad
+    GPIO_ConfigurePin(UART2_TX_PORT, UART2_TX_PIN, OUTPUT, PID_UART2_TX, false);
+#endif
+
+}
+
+#if defined (CFG_PRINTF_UART2)
+// Configuration struct for UART2
+static const uart_cfg_t uart_cfg = {
+    .baud_rate = UART2_BAUDRATE,
+    .data_bits = UART2_DATABITS,
+    .parity = UART2_PARITY,
+    .stop_bits = UART2_STOPBITS,
+    .auto_flow_control = UART2_AFCE,
+    .use_fifo = UART2_FIFO,
+    .tx_fifo_tr_lvl = UART2_TX_FIFO_LEVEL,
+    .rx_fifo_tr_lvl = UART2_RX_FIFO_LEVEL,
+    .intr_priority = 2,
+};
+#endif
+
+void periph_init(void)
+{
+#if defined (__DA14531__)
+    // In Boost mode enable the DCDC converter to supply VBAT_HIGH for the used GPIOs
+    syscntl_dcdc_turn_on_in_boost(SYSCNTL_DCDC_LEVEL_3V0);
+#else
+    // Power up peripherals' power domain
+    SetBits16(PMU_CTRL_REG, PERIPH_SLEEP, 0);
+    while (!(GetWord16(SYS_STAT_REG) & PER_IS_UP));
+    SetBits16(CLK_16M_REG, XTAL16_BIAS_SH_ENABLE, 1);
+#endif
+
+    // ROM patch
+    patch_func();
+
+    // Initialize peripherals
+#if defined (CFG_PRINTF_UART2)
+    // Initialize UART2
+    uart_initialize(UART2, &uart_cfg);
+#endif
+
+    // Set pad functionality
+    set_pad_functions();
+
+    // Enable the pads
+    GPIO_set_pad_latch_en(true);
+}
---
+++ features/empty_peripheral_eclipse/src/user_empty_peripheral_template.c
@@ -0,0 +1,49 @@
+/**
+ ****************************************************************************************
+ *
+ * @file user_empty_peripheral_template.c
+ *
+ * @brief Empty peripheral template project source code.
+ *
+ * Copyright (C) 2012-2019 Dialog Semiconductor.
+ * This computer program includes Confidential, Proprietary Information
+ * of Dialog Semiconductor. All Rights Reserved.
+ *
+ ****************************************************************************************
+ */
+
+/**
+ ****************************************************************************************
+ * @addtogroup APP
+ * @{
+ ****************************************************************************************
+ */
+#include "rwip_config.h"             // SW configuration
+
+
+/*
+ * INCLUDE FILES
+ ****************************************************************************************
+ */
+
+#include "app_api.h"
+#include "user_empty_peripheral_template.h"
+
+/*
+ * FUNCTION DEFINITIONS
+ ****************************************************************************************
+*/
+
+
+void user_on_connection(uint8_t connection_idx, struct gapc_connection_req_ind const *param)
+{
+    default_app_on_connection(connection_idx, param);
+}
+
+void user_on_disconnect( struct gapc_disconnect_ind const *param )
+{
+    default_app_on_disconnect(param);
+}
+ 
+
+/// @} APP

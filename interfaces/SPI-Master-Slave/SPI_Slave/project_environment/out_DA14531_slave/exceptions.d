577fa9690cf7bf6486ddb73b5d0b11ada949a0bc
spuranik <svdpuranik@gmail.com>
2023-02-20 01:55:09 +0100

    - Build issue solved

--- interfaces/SPI-Master-Slave/SPI_Slave/project_environment/out_DA14531_slave/exceptions.d
+++
@@ -1,1 +0,0 @@
-.\out_da14531_slave\exceptions.o: ..\..\..\..\..\..\\projects\target_apps\peripheral_examples\shared\source\exceptions.c
--- interfaces/SPI-Master-Slave/SPI_Slave/project_environment/out_DA14531_slave/startup_da14531.d
+++
@@ -1,1 +0,0 @@
-.\out_da14531_slave\startup_da14531.o: ..\..\..\..\..\..\\sdk\platform\arch\boot\ARM\startup_DA14531.s

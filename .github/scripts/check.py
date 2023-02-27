6efd19837b37b79c9a92fd75f47dc25f5f16e2e8
koenvdm <koen.van-den-maagdenberg.xn@renesas.com>
2023-02-27 14:48:03 +0000

    change find-failing.sh to check.py for consistency and for json support

---
+++ .github/scripts/check.py
@@ -0,0 +1,47 @@
+import os
+import subprocess
+
+class Target:
+    def __init__(self, name):
+        self.name = name
+        self.passed = []
+        self.failed = []
+
+targets=[Target("531"), Target("585"), Target("586")]
+
+def bash(command):
+    if type(command) is str:
+        command = command.split()
+    else:
+        command = list(command)
+    process = subprocess.Popen(command, stdout=subprocess.PIPE)
+    return process.communicate()[0], process.returncode
+
+if __name__ == "__main__":
+    workdir = os.getenv('GITHUB_WORKSPACE',"./")
+
+    with open(workdir+".github/config/build-list.txt") as f:
+        exlist = f.read().splitlines()
+
+    for d in exlist:
+        exfolder = bash('dirname '+d)[0].decode("utf-8").rstrip()
+        exname = bash('basename '+exfolder)[0].decode("utf-8").rstrip()
+
+        for t in targets:
+            if (bash(["grep","-q","set(BUILD_FOR_"+t.name+" TRUE)",exfolder+"/CMakeLists.txt"])[1] == 0):
+                if(bash("test -f "+exfolder+"/build/"+exname+"_"+t.name+".elf")[1] == 0):
+                    t.passed.append(exfolder)
+                else:
+                    t.failed.append(exfolder)
+
+for t in targets:
+    print("\npassed "+t.name+":")
+    for p in t.passed:
+        print(p)
+    print("\nfailed "+t.name+":")
+    for p in t.failed:
+        print(p)
+    print("\n---------------")
+    print("| PASSED: "+str(len(t.passed))+" ")
+    print("| FAILED: "+str(len(t.failed))+" ")
+    print("---------------")

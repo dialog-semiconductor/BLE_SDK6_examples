ec6d6ac20b71276bfb7f74634d1f5fe8076ce50f
kvandenm <koen.van-den-maagdenberg.xn@renesas.com>
2023-03-15 17:03:58 +0100

    update copyright

--- connectivity/ble_weight_scale_nau7802/src/config/user_config.h
+++ connectivity/ble_weight_scale_nau7802/src/config/user_config.h
@@ -5,24 +5,16 @@
  *
  * @brief User configuration file.
  *
- * Copyright (c) 2023 Dialog Semiconductor. All rights reserved.
- * 
- * This software ("Software") is owned by Dialog Semiconductor. By using this Software
- * you agree that Dialog Semiconductor retains all intellectual property and proprietary
- * rights in and to this Software and any use, reproduction, disclosure or distribution
- * of the Software without express written permission or a license agreement from Dialog
- * Semiconductor is strictly prohibited. This Software is solely for use on or in
- * conjunction with Dialog Semiconductor products.
- * 
- * EXCEPT AS OTHERWISE PROVIDED IN A LICENSE AGREEMENT BETWEEN THE PARTIES OR AS
- * REQUIRED BY LAW, THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. EXCEPT AS OTHERWISE PROVIDED
- * IN A LICENSE AGREEMENT BETWEEN THE PARTIES OR BY LAW, IN NO EVENT SHALL DIALOG
- * SEMICONDUCTOR BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT, INCIDENTAL, OR
- * CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
- * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
- * ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THE SOFTWARE.
+ * The MIT License (MIT)
+ * Copyright (c) 2023 Renesas Electronics Corporation and/or its affiliates
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
  *
  ***************************************************************************************
  */

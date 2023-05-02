cf9231976ad50813fac378bb006dcc6c39aa8c06
kvandenm <koen.van-den-maagdenberg.xn@renesas.com>
2023-05-02 12:42:04 +0000

    split gcc and clang test build

---
+++ interfaces/simple_button/testbuildclang.sh
@@ -0,0 +1,12 @@
+rm -rf build
+
+cmake -DDEVICE_NAME="simple_button" \
+    -DCMAKE_BUILD_TYPE=DEBUG \
+    -DCMAKE_TOOLCHAIN_FILE="/root/SDK6.1182.1/projects/BLE_SDK6_examples/build_utils/gcc/clang.cmake" \
+    -DDIALOG_SDK_PATH="/root/SDK6.1182.1" \
+    -DDIALOG_EXAMPLE_PATH="/root/SDK6.1182.1/projects/BLE_SDK6_examples" \
+    -S "." \
+    -B "build"
+
+cd build
+make -j 7
--- interfaces/simple_button/testbuildgcc.sh
+++ interfaces/simple_button/testbuildgcc.sh
@@ -1,12 +1,14 @@
 rm -rf build
 
-cmake -DDEVICE_NAME="active_scanner" \
+cmake -DDEVICE_NAME="simple_button" \
     -DCMAKE_BUILD_TYPE=DEBUG \
-    -DCMAKE_TOOLCHAIN_FILE="/root/SDK6.1182.1/projects/BLE_SDK6_examples/build_utils/gcc/clang.cmake" \
+    -DCMAKE_TOOLCHAIN_FILE="/root/SDK6.1182.1/projects/BLE_SDK6_examples/build_utils/gcc/arm-none-eabi.cmake" \
+    -DGCC_TOOLCHAIN_PATH="/usr/bin/arm-none-eabi-gcc" \
     -DDIALOG_SDK_PATH="/root/SDK6.1182.1" \
     -DDIALOG_EXAMPLE_PATH="/root/SDK6.1182.1/projects/BLE_SDK6_examples" \
     -S "." \
     -B "build"
+
 
 cd build
 make -j 7

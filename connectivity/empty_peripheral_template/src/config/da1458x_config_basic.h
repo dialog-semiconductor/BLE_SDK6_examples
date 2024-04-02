9aaf373e49b2d00c5a2f108baa7758bfb06b32ff
Mohammad Seyed <Mohammad.Seyed@diasemi.com>
2024-04-02 14:32:26 +0200

    for merging to sdk6022_migration

---
+++ connectivity/empty_peripheral_template/src/config/da1458x_config_basic.h
@@ -0,0 +1,45 @@
+/**
+ ****************************************************************************************
+ *
+ * @file da1458x_config_basic.h
+ *
+ * @brief Basic compile configuration file.
+ *
+ * Copyright (C) 2014-2023 Renesas Electronics Corporation and/or its affiliates.
+ * All rights reserved. Confidential Information.
+ *
+ * This software ("Software") is supplied by Renesas Electronics Corporation and/or its
+ * affiliates ("Renesas"). Renesas grants you a personal, non-exclusive, non-transferable,
+ * revocable, non-sub-licensable right and license to use the Software, solely if used in
+ * or together with Renesas products. You may make copies of this Software, provided this
+ * copyright notice and disclaimer ("Notice") is included in all such copies. Renesas
+ * reserves the right to change or discontinue the Software at any time without notice.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS". RENESAS DISCLAIMS ALL WARRANTIES OF ANY KIND,
+ * WHETHER EXPRESS, IMPLIED, OR STATUTORY, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. TO THE
+ * MAXIMUM EXTENT PERMITTED UNDER LAW, IN NO EVENT SHALL RENESAS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE, EVEN IF RENESAS HAS BEEN ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGES. USE OF THIS SOFTWARE MAY BE SUBJECT TO TERMS AND CONDITIONS CONTAINED IN
+ * AN ADDITIONAL AGREEMENT BETWEEN YOU AND RENESAS. IN CASE OF CONFLICT BETWEEN THE TERMS
+ * OF THIS NOTICE AND ANY SUCH ADDITIONAL LICENSE AGREEMENT, THE TERMS OF THE AGREEMENT
+ * SHALL TAKE PRECEDENCE. BY CONTINUING TO USE THIS SOFTWARE, YOU AGREE TO THE TERMS OF
+ * THIS NOTICE.IF YOU DO NOT AGREE TO THESE TERMS, YOU ARE NOT PERMITTED TO USE THIS
+ * SOFTWARE.
+ *
+ ****************************************************************************************
+ */
+
+#ifndef _DA1458X_CONFIG_BASIC_H_
+#define _DA1458X_CONFIG_BASIC_H_
+
+#if defined (__DA14535__)
+    #include "da14535_config_basic.h"
+#elif defined (__DA14531__)
+    #include "da14531_config_basic.h"
+#else
+    #include "da14585_config_basic.h"
+#endif
+
+#endif // _DA1458X_CONFIG_BASIC_H_

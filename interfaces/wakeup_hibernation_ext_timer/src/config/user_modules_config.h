ca3888f9dbd64ae68c5498fe8c17c9e6e50248c1
Mohammad Seyed <mohammad.seyed.xv@renesas.com>
2024-07-30 13:09:16 +0200

    external timer hibernation

---
+++ interfaces/wakeup_hibernation_ext_timer/src/config/user_modules_config.h
@@ -0,0 +1,79 @@
+/**
+ ****************************************************************************************
+ *
+ * @file user_modules_config.h
+ *
+ * @brief User modules configuration file.
+ *
+ * Copyright (C) 2015-2023 Renesas Electronics Corporation and/or its affiliates.
+ * All rights reserved. Confidential Information.
+ *
+ * This software ("Software") is supplied by Renesas Electronics Corporation and/or its
+ * affiliates ("Renesas"). Renesas grants you a personal, non-exclusive, non-transferable,
+ * revocable, non-sub-licensable right and license to use the Software, solely if used in
+ * or together with Renesas products. You may make copies of this Software, provided this
+ * copyright notice and disclaimer ("Notice") is included in all such copies. Renesas
+ * reserves the right to change or discontinue the Software at any time without notice.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS". RENESAS DISCLAIMS ALL WARRANTIES OF ANY KIND,
+ * WHETHER EXPRESS, IMPLIED, OR STATUTORY, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. TO THE
+ * MAXIMUM EXTENT PERMITTED UNDER LAW, IN NO EVENT SHALL RENESAS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE, EVEN IF RENESAS HAS BEEN ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGES. USE OF THIS SOFTWARE MAY BE SUBJECT TO TERMS AND CONDITIONS CONTAINED IN
+ * AN ADDITIONAL AGREEMENT BETWEEN YOU AND RENESAS. IN CASE OF CONFLICT BETWEEN THE TERMS
+ * OF THIS NOTICE AND ANY SUCH ADDITIONAL LICENSE AGREEMENT, THE TERMS OF THE AGREEMENT
+ * SHALL TAKE PRECEDENCE. BY CONTINUING TO USE THIS SOFTWARE, YOU AGREE TO THE TERMS OF
+ * THIS NOTICE.IF YOU DO NOT AGREE TO THESE TERMS, YOU ARE NOT PERMITTED TO USE THIS
+ * SOFTWARE.
+ *
+ ****************************************************************************************
+ */
+
+#ifndef _USER_MODULES_CONFIG_H_
+#define _USER_MODULES_CONFIG_H_
+
+/**
+ ****************************************************************************************
+ * @addtogroup APP
+ * @ingroup RICOW
+ *
+ * @brief User modules configuration.
+ *
+ * @{
+ ****************************************************************************************
+ */
+
+/*
+ * DEFINES
+ ****************************************************************************************
+ */
+
+/***************************************************************************************/
+/* Exclude or not a module in user's application code.                                 */
+/*                                                                                     */
+/* (0) - The module is included. The module's messages are handled by the SDK.         */
+/*                                                                                     */
+/* (1) - The module is excluded. The user must handle the module's messages.           */
+/*                                                                                     */
+/* Note:                                                                               */
+/*      This setting has no effect if the respective module is a BLE Profile           */
+/*      that is not used included in the user's application.                           */
+/***************************************************************************************/
+#define EXCLUDE_DLG_GAP             (0)
+#define EXCLUDE_DLG_TIMER           (0)
+#define EXCLUDE_DLG_MSG             (1)
+#define EXCLUDE_DLG_SEC             (1)
+#define EXCLUDE_DLG_DISS            (1)
+#define EXCLUDE_DLG_PROXR           (1)
+#define EXCLUDE_DLG_BASS            (1)
+#define EXCLUDE_DLG_FINDL           (1)
+#define EXCLUDE_DLG_FINDT           (1)
+#define EXCLUDE_DLG_SUOTAR          (1)
+#define EXCLUDE_DLG_CUSTS1          (1)
+#define EXCLUDE_DLG_CUSTS2          (1)
+
+/// @} APP
+
+#endif // _USER_MODULES_CONFIG_H_
---
+++ interfaces/wakeup_hibernation_ext_timer/src/user_hibernation_timer.h
@@ -0,0 +1,110 @@
+/**
+ ****************************************************************************************
+ *
+ * @file user_hibernation_timer.h
+ *
+ * @brief BLE Eddystone beacon with hibernation or deep sleep project header file. 
+ *
+ * Copyright (c) 2015-2021 Renesas Electronics Corporation and/or its affiliates
+ * The MIT License (MIT)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ ****************************************************************************************
+ */
+
+#ifndef _USER_HIBERNATION_TIMER_H_
+#define _USER_HIBERNATION_TIMER_H_
+
+/**
+ ****************************************************************************************
+ * @addtogroup APP
+ * @ingroup
+ *
+ * @brief
+ *
+ * @{
+ ****************************************************************************************
+ */
+
+/*
+ * INCLUDE FILES
+ ****************************************************************************************
+ */
+ 
+#include "gapc_task.h"                 // gap functions and messages
+#include "app_task.h"                  // application task
+#include "app.h"                       // application definitions
+#include "app_callback.h"
+#include "arch_api.h"
+
+/*
+ * DEFINES
+ ****************************************************************************************
+ */
+
+/* Define hibernation mode */
+#undef CFG_HIBERNATION_MODE
+#define  CFG_DEEP_SLEEP_MODE
+
+#if defined(CFG_HIBERNATION_MODE) && defined(CFG_DEEP_SLEEP_MODE)
+#error "Config error: CFG_HIBERNATION_MODE and CFG_DEEP_SLEEP_MODE cannot be defined at the same time"
+#endif
+
+/*
+ * FUNCTION DECLARATIONS
+ ****************************************************************************************
+ */
+
+/**
+ ****************************************************************************************
+ * @brief Application initialization function.
+ * @return void
+ ****************************************************************************************
+*/
+void user_app_on_init(void);
+
+/**
+ ****************************************************************************************
+ * @brief Advertising function.
+ * @return void
+ ****************************************************************************************
+*/
+void user_app_adv_start(void);
+
+/**
+ ****************************************************************************************
+ * @brief Undirect non-connectable advertising completion function.
+ * @param[in] status Command complete event message status
+ * @return void
+ ****************************************************************************************
+*/
+void user_app_on_adv_nonconn_complete(uint8_t status);
+
+/**
+ ****************************************************************************************
+ * @brief Function called when BLE is powered. 
+ * @return GOTO_SLEEP
+ * @note Return type allows the device to enter sleep.
+ ****************************************************************************************
+*/
+arch_main_loop_callback_ret_t user_on_ble_powered(void);
+
+/// @} APP
+
+#endif //_USER_HIBERNATION_TIMER_H_
